1d4b8fea33896f2bd1f53724e421e9d2e4d69e1a
alsqja <alsqha2626@gmail.com>
2023-03-02 14:15:41 +0900

    Add first setting

---
+++ server/app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+// const path = require('path');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const passport = require('passport');
+
+// const indexRouter = require('./routes');
+// const passportConfig = require('./passport');
+
+dotenv.config();
+
+const app = express();
+// passportConfig();
+app.set('port', process.env.PORT || 8080);
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+// app.use('/', indexRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  console.log(error);
+  next(error);
+});
+
+app.use((err, req, res) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  console.error(err);
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
+});
