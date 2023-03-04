32aec6056077416b65ba2541f585b3946432adf3
alsqja <alsqha2626@gmail.com>
2023-03-04 15:40:34 +0900

    add create chats

--- server/controller/Chat/index.js
+++ server/controller/Chat/index.js
@@ -1,6 +1,28 @@
+const {isAuthorized} = require('../../utils/tokenFunctions')
+const {chats} = require('../../models/index')
+
 module.exports = {
   post: async(req, res, next) => {
-    const {title, contents, private} = req.body;
+    const { title, contents, private } = req.body;
+    const accessTokenData = isAuthorized(req);
+    const {id} = accessTokenData
 
+    try {
+      const newChats = await chats.create({
+        contents,
+        user_id: id,
+        title,
+        private,
+      })
+
+      return res.status(201).send({
+        data: newChats,
+        message: '채팅방이 생성되었습니다.'
+      })
+    }
+    catch(e) {
+      console.error(e)
+      return next(e)
+    }
   }
 }
